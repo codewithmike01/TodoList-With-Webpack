8f532580297ba9a8be5d58e630561a70bac661d8
ginohmk <michotall95@gmail.com>
2022-01-11 14:50:40 +0100

    Add assets to dist with generator plugin and extracted css with miniExtracCss plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main[contentHash].js',
+    filename: 'main[hash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
@@ -16,13 +18,26 @@ module.exports = {
       titile: 'Output management',
       template: './src/index.html',
     }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
   ],
 
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][hash][ext]',
+        },
       },
     ],
   },
