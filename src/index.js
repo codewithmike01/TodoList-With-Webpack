c9ae5cb458caa5a34845c82b60243c2ff18f40a1
ginohmk <michotall95@gmail.com>
2022-01-11 18:33:00 +0100

    Add dynamic html  todo list and styleing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './index.css';
+
+const todoListContianer = document.querySelector('.todo__list');
+
+// const li = document.createElement('li');
+
+const listTodo = [
+  { description: 'wash the dishes', completed: false, index: 0 },
+  { description: 'Complete To Do list project', completed: false, index: 1 },
+  { description: 'Fix Car', completed: false, index: 2 },
+];
+
+listTodo.forEach((list) => {
+  todoListContianer.innerHTML += `<li>
+  <div class="check">
+    <input type="checkbox" name="completed" />
+    <p>${list.description}</p>
+  </div>
+  <div class="three__dots">
+  </div>
+</li>
+<hr /> `;
+});
